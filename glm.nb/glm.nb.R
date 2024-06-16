d0a0479696cfcfda4c015746c2c3df7d13a017e0
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-06-16 22:03:53 +0700

    Use glm.nb With (In) R Software

--- glm.nb/glm.nb.R
+++ glm.nb/glm.nb.R
@@ -4,7 +4,7 @@
 # Fit a Negative Binomial Generalized Linear Model Use glm.nb With (In) R Software
 install.packages("MASS")
 library("MASS")
-spml_one = read.csv("https://raw.githubusercontent.com/timbulwidodostp/glm.nb/main/glm.nb/glm.nb.csv",sep = ";")
+glm.nb = read.csv("https://raw.githubusercontent.com/timbulwidodostp/glm.nb/main/glm.nb/glm.nb.csv",sep = ";")
 # Estimation Fit a Negative Binomial Generalized Linear Model Use glm.nb With (In) R Software
 glm.nb <- glm.nb(Days ~ Sex/(Age + Eth*Lrn), data = glm.nb)
 summary(glm.nb)
@@ -13,6 +13,3 @@ summary(glm.nb)
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
 # Finished
-
-
-
